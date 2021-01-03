61159e49f943684c941ff2d9b2505a3b665fba8a
Denis Rodionov <nicholas.hawkwood@gmail.com>
2021-01-03 16:25:04 +0200

    Year rendering helper

---
+++ Helpers/StringHelpers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Helpers
+{
+    public static class StringHelpers
+    {
+        public static string FormatYearsRange(int startYear, int? finishYear)
+        {
+            var startYearStr = startYear.ToString();
+            var finishYearStr = finishYear.ToString();
+            var startYearOutput = $"{startYearStr}&ndash;";
+
+            if (startYearStr.Length != 4) throw new ArgumentException($"Start year should consist of 4 digits, got {startYear}");
+
+            if (!finishYear.HasValue) return startYearOutput;
+
+            return startYearStr.Substring(0, 2).Equals(finishYearStr.Substring(0, 2))
+                ? $"{startYearOutput}{finishYearStr.Substring(finishYearStr.Length - 2)}"
+                : $"{startYearOutput}{finishYearStr}";
+        }
+        
+        public static string FormatYearsRange(int? startYear, int? finishYear)
+        {
+            var startYearStr = startYear.ToString();
+            var finishYearStr = finishYear.ToString();
+
+            if (startYear.HasValue && !finishYear.HasValue) return startYearStr;
+            if (!startYear.HasValue && finishYear.HasValue) return finishYearStr;
+            if (!startYear.HasValue && !finishYear.HasValue) return "";
+            
+            return startYearStr.Substring(0, 2).Equals(finishYearStr.Substring(0, 2))
+                ? $"{startYearStr}&ndash;{finishYearStr.Substring(finishYearStr.Length - 2)}"
+                : $"{startYearStr}&ndash;{finishYearStr}";
+            
+        }
+    }
+}
